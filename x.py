3ebcb9257433a7d1a12c6f1d5cfced6e1ab9e16b
iaan0607 <149846606+iaan0607@users.noreply.github.com>
2023-11-11 10:35:01 +0900

    sports.py

--- x.py
+++ x.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title('portpolio')
+
+tab1,tab2,tab3,tab4 = st.tabs(["Football","Basketball","Baseball","Boxing"])
+
+with tab1:
+  st.header("Football")
+with tab2:
+  st.header("Basketball")
+with tab3:
+  st.header("Baseball")
+with tab4:
+  st.header("Boxing")
+
+ 
