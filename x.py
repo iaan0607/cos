5b38f0a6c2620503fb14aa296a4cc605c19c9a41
iaan0607 <149846606+iaan0607@users.noreply.github.com>
2023-11-11 10:41:19 +0900

    x.py

--- x.py
+++ x.py
@@ -3,16 +3,18 @@ import pandas as pd
 import numpy as np
 
 st.title('portpolio')
-
-tab1,tab2,tab3,tab4 = st.tabs(["Football","Basketball","Baseball","Boxing"])
-
-with tab1:
-  st.header("Football")
+tab1,tab2 = st.tabs(["about me","sports"])
 with tab2:
-  st.header("Basketball")
-with tab3:
-  st.header("Baseball")
-with tab4:
-  st.header("Boxing")
+  st.header("sports")
+  tab1,tab2,tab3,tab4 = st.tabs(["Football","Basketball","Baseball","Boxing"])
+
+  with tab1:
+    st.header("Football")
+  with tab2:
+    st.header("Basketball")
+  with tab3:
+    st.header("Baseball")
+  with tab4:
+    st.header("Boxing")
 
  
