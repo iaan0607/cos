e58df63f57ef05ad4aec46f3955c3a2149155ff5
iaan0607 <149846606+iaan0607@users.noreply.github.com>
2023-11-23 19:20:10 +0900

    Update x.py

--- x.py
+++ x.py
@@ -9,26 +9,17 @@ with tab1:
   st.header("i am human")
   
 with tab2:
-  st.header("sports")
-  tab1,tab2,tab3,tab4 = st.tabs(["Football","Basketball","Baseball","Boxing"])
-
-  with tab1:
-    st.header("Football")
-    taba,tabb,tabc,tabd = st.tabs(["EPL","LALIGA","SERIE A","K LEAGUE"])
-    with taba:
-      st.header("EPL")
-    with tabb:
-      st.header("LALIGA")
-    with tabc:
-      st.header("SERIE A")
-    with tabd:
-      st.header("K LEAGUE")
+  st.header("football")
+  taba,tabb,tabc,tabd = st.tabs(["EPL","LALIGA","SERIE A","K LEAGUE"])
+  with taba:
+  st.header("EPL")
+  with tabb:
+  st.header("LALIGA")
+  with tabc:
+  st.header("SERIE A")
+  with tabd:
+  st.header("K LEAGUE")
       
-  with tab2:
-    st.header("Basketball")
-  with tab3:
-    st.header("Baseball")
-  with tab4:
-    st.header("Boxing")
+ 
 
  
